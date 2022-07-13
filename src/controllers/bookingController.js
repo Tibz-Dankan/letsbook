fd7621d24668191b2f2e55f8975c4bd6b037fc2c
Tibz Dankan <tibesigwadankan@gmail.com>
2022-07-13 03:24:43 +0300

    feat: logic to return users booking details

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -19,32 +19,126 @@ const bookingDates = async (req, res, next) => {
   }
 };
 
+const numberOfDays = (stringifiedDateObject) => {
+  const day = 1000 * 60 * 60 * 24;
+  const date = JSON.parse(stringifiedDateObject).date;
+  const numOfDays = (new Date(date) - new Date(Date.now())) / day;
+  return Math.floor(numOfDays);
+};
+
+const currentBookingData = (bookingsArray, currentBookingId) => {
+  const bookingData = bookingsArray.find((booking) => {
+    return booking.booking_id === parseInt(currentBookingId);
+  });
+  return bookingData;
+};
+
+const sortRoomIds = (bookingsArray, myCheckInDays, myCheckOutDays) => {
+  let bookedRoomId = [];
+  bookingsArray.forEach(({ check_in_date, room_id }) => {
+    if (
+      myCheckInDays >= numberOfDays(check_in_date) &&
+      numberOfDays(check_in_date) <= myCheckOutDays &&
+      room_id !== null
+    ) {
+      bookedRoomId.push(room_id);
+    }
+  });
+  return bookedRoomId;
+};
+
+const sortRoomsSendResponse = (roomsArray, sortedRoomIdArray, res) => {
+  // TODO: perfect this algorithm
+  let room = [];
+  roomsArray.forEach(
+    ({ room_id, room_description, no_of_beds, price, image_url }) => {
+      const roomObject = {
+        // TODO: add column room_name to room table
+        room_id: room_id,
+        room_description: room_description,
+        no_of_beds: no_of_beds,
+        price: price,
+        image_url: image_url,
+      };
+      if (!sortedRoomIdArray[0]) {
+        room.push(roomObject);
+      } else {
+        sortedRoomIdArray.map((roomId) => {
+          if (roomId !== room_id) {
+            room.push(roomObject);
+          }
+        });
+      }
+    }
+  );
+  return res.status(200).json(room);
+};
+
 const getUnbookedRooms = async (req, res, next) => {
   const currentBookingId = req.params.booking_id;
   if (!currentBookingId) {
     return res.json({ errorMessage: "No booking id provided" });
   }
-  // select all bookings
   const bookings = await Booking.getAllBookings();
-  console.log(bookings.rows);
-  // filter out the current booking details(checkIn and checkOut date)
-  const currentBookingData = bookings.rows.find((booking) => {
-    return booking.booking_id === currentBookingId;
-  });
-  console.log(currentBookingData);
-  // return;
+  const rooms = await Room.getAllRooms();
+
+  const myBookingData = currentBookingData(bookings.rows, currentBookingId);
+  const myCheckInDaysFromNow = numberOfDays(myBookingData.check_in_date);
+  const myCheckOutDaysFromNow = numberOfDays(myBookingData.check_out_date);
+
+  const bookedRoomIdsArray = sortRoomIds(
+    bookings.rows,
+    myCheckInDaysFromNow,
+    myCheckOutDaysFromNow
+  );
+  sortRoomsSendResponse(rooms.rows, bookedRoomIdsArray, res);
 };
 
-// Function to be tested
 const bookRoom = async (req, res, next) => {
   const bookingId = req.params.booking_id;
-  const roomId = req.params.room_id;
+  const roomId = req.body.roomId;
   if (!bookingId || !roomId) {
     return res.json({ errorMessage: "No booking id or no room id" });
   }
   await Booking.updateBookingWithRoom(bookingId, roomId);
-  await Room.updateRoomWithBooking(roomId, bookingId);
   res.status(200).json({ status: "success" });
 };
 
-module.exports = { bookingDates, bookRoom, getUnbookedRooms };
+const matchBookingToRoomSendResponse = (bookingsArray, res) => {
+  const bookingData = [];
+  bookingsArray.map(async (booking, index) => {
+    if (booking.room_id !== null) {
+      const room = await Room.getRoomByRoomId(booking.room_id);
+      bookingData.push({
+        booking_id: booking.booking_id,
+        check_in_date: booking.check_in_date,
+        check_out_date: booking.check_out_date,
+        booking_date: booking.booking_date,
+        room_id: booking.room_id,
+        room_id: booking.room_id,
+        room_name: room.rows[0].room_name,
+        price: room.rows[0].price,
+        no_of_beds: room.rows[0].no_of_beds,
+        has_paid: booking.has_paid,
+        is_cancelled: booking.is_cancelled,
+      });
+    }
+    if (bookingsArray.length === index + 1) {
+      return res.status(200).json(bookingData);
+    }
+  });
+};
+
+const getMyBookings = async (req, res, next) => {
+  const userId = req.params.user_id;
+  if (!userId) {
+    return res.json({ errorMessage: "No user id, refresh to continue" });
+  }
+  const booking = await Booking.getBookingByUserId(userId);
+  if (!booking.rows[0]) {
+    return res.json({ errorMessage: "You have no booking yet" });
+  }
+  matchBookingToRoomSendResponse(booking.rows, res);
+};
+
+module.exports = { bookingDates, getUnbookedRooms, bookRoom, getMyBookings };
--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -3,13 +3,15 @@ const {
   bookingDates,
   bookRoom,
   getUnbookedRooms,
+  getMyBookings,
 } = require("../controllers/bookingController");
 const { verifyJwtToken } = require("../utils/verifyJwtToken");
 
 const router = express.Router();
 
 router.post("/booking-date/:user_id", verifyJwtToken, bookingDates);
-router.post("/book-room/:booking_id/:room_id", verifyJwtToken, bookRoom);
 router.get("/get-unbooked-rooms/:booking_id", verifyJwtToken, getUnbookedRooms);
+router.post("/book-room/:booking_id", verifyJwtToken, bookRoom);
+router.get("/get-my-bookings/:user_id", verifyJwtToken, getMyBookings);
 
 module.exports = router;
