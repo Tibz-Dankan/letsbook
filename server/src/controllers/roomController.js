45b393427164b10779016f5d4e6c3df47afb0d1e
Tibz Dankan <tibesigwadankan@gmail.com>
2022-07-24 22:57:30 +0300

    feat: image upload to the disk storage

--- server/src/controllers/roomController.js
+++ server/src/controllers/roomController.js
@@ -1,4 +1,5 @@
 const Room = require("../models/room");
+const multer = require("multer");
 
 const createARoom = async (req, res, next) => {
   if (!req.body) return res.json({ errorMessage: "No room details provided" });
@@ -11,4 +12,19 @@ const createARoom = async (req, res, next) => {
   res.status(200).json({ status: "success" });
 };
 
-module.exports = { createARoom };
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./src/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const uploadImage = async (req, res) => {
+  res.status(200).json({ status: "success" });
+};
+
+module.exports = { createARoom, upload, uploadImage };
--- server/src/routes/roomRoutes.js
+++ server/src/routes/roomRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const { createARoom } = require("../controllers/roomController");
+const {
+  createARoom,
+  upload,
+  uploadImage,
+} = require("../controllers/roomController");
 const { verifyJwtToken } = require("../utils/verifyJwtToken");
 
 const router = express.Router();
 
 router.post("/add-room", verifyJwtToken, createARoom);
+router.post(
+  "/upload-image",
+  verifyJwtToken,
+  upload.single("file"),
+  uploadImage
+);
 
 module.exports = router;
