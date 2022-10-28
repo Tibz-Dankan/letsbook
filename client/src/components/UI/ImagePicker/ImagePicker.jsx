6861c65808b9865d65fc3d580a445465826f5651
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-10-28 14:34:07 +0300

    refactor(image Picker):logic simplified by using a library

--- client/src/components/UI/ImagePicker/ImagePicker.jsx
+++ client/src/components/UI/ImagePicker/ImagePicker.jsx
@@ -1,94 +1,76 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
+import { useFilePicker } from "use-file-picker";
 import styles from "./ImagePicker.module.scss";
 
 const ImagePicker = (props) => {
-  const [photo, setPhoto] = useState({ preview: "", data: "" });
-  const [imageErrorMsg, setImageErrorMsg] = useState("");
-  const [isImageError, setIsImageError] = useState(false);
-  let isValidImgType;
+  const [image, setImage] = useState(null);
+  const [isDoneImageReview, setIsDoneImageReview] = useState(false);
 
-  const handleImageChange = (event) => {
-    const image = {
-      preview: URL.createObjectURL(event.target.files[0]),
-      data: event.target.files[0],
-    };
-    console.log(image);
-    setPhoto(image);
-    validateImageFile();
-    handleSelect();
-  };
+  const [openFileSelector, { filesContent, loading, errors }] = useFilePicker({
+    readAs: "DataURL",
+    accept: "image/*",
+    multiple: false,
+    limitFilesConfig: { max: 1 },
+    maxFileSize: 5,
+  });
 
-  const isValidImageType = (imageType) => {
-    isValidImgType =
-      imageType === "image/jpeg" ||
-      imageType === "image/jpg" ||
-      imageType === "image/png";
-    if (isValidImgType) return true;
-    setImageErrorMsg(
-      "Only accept image files that end with .jpeg, .jpg and .png"
-    );
-    // setPhoto({ preview: "", data: "" });
-    setIsImageError(true);
-    setTimeout(() => {
-      setImageErrorMsg("");
-      setIsImageError(false);
-    }, 5000);
-    return false;
-  };
+  useEffect(() => {
+    filesContent.map((file, index) => {
+      console.log(file);
+      return setImage(file.content);
+    });
+  }, [filesContent]);
 
-  const isValidImageSize = (imageSize) => {
-    const maximumAcceptableImageSize = 5242880; // 5mb
-    if (imageSize < maximumAcceptableImageSize) return true;
-    setImageErrorMsg("Only accept image size less than 5mb");
-    // setPhoto({ preview: "", data: "" });
-    setIsImageError(true);
-    setTimeout(() => {
-      setImageErrorMsg("");
-      setIsImageError(false);
-    }, 5000);
-    return false;
-  };
-
-  const validateImageFile = () => {
-    console.log(photo);
-    const imageType = photo.data.type;
-    const imageSize = photo.data.size;
-    return isValidImageType(imageType) && isValidImageSize(imageSize);
-  };
-
-  const handleSelect = () => {
-    props.onSelect(photo);
+  const imageSaveHandler = () => {
+    props.onSave(image);
   };
 
   return (
     <Fragment>
-      <div className={styles["image__picker"]}>
-        {/* {isValidImgType && photo.preview && ( */}
-        {photo.preview && (
-          <img src={photo.preview} width="100px" height="100px" alt="Room" />
-        )}
-        {isImageError && (
-          <span className={styles["image__picker__error"]}>
-            {imageErrorMsg}
-          </span>
-        )}
-        {!photo.preview && (
-          <div className={styles["image__picker__input"]}>
-            <input
-              type="file"
-              name="file"
-              className={styles["image__picker__input__field"]}
-              onChange={(event) => handleImageChange(event)}
-              required
-            />
-          </div>
-        )}
-        {photo.preview && (
-          <span onClick={() => setPhoto((prevPhotoData) => prevPhotoData)}>
-            DONE
-          </span>
-        )}
-      </div>
+      {!isDoneImageReview && (
+        <div className={styles["image__picker"]}>
+          {errors.length && (
+            <div className={styles["image__picker__error"]}>
+              <span>Error occurred when selecting image</span>
+            </div>
+          )}
+          {loading && (
+            <div className={styles["image__picker__loading"]}>
+              <span>Loading...</span>
+            </div>
+          )}
+          {!image && (
+            <div className={styles["image__picker__container"]}>
+              <span className={styles["image__picker__heading"]}>
+                Please Select {props.category} Image
+              </span>
+              <button onClick={() => openFileSelector()}>Select Image</button>
+            </div>
+          )}
+          {image && (
+            <div className={styles["image__picker__review"]}>
+              <span className={styles["image__picker__review--msg"]}>
+                Review Selected Image
+              </span>
+              <img
+                src={image}
+                alt="selected imageFile"
+                className={styles["image__picker__review--image"]}
+              />
+              <button
+                className={styles["image__picker__review--done"]}
+                onClick={() => {
+                  setIsDoneImageReview(true);
+                  imageSaveHandler();
+                  setImage("");
+                }}
+              >
+                Done
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </Fragment>
   );
 };
