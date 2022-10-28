b3e719f6ab6124bd74d3e0f4686d723e528cbf71
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-10-28 14:43:52 +0300

    refactor: add room logic simplified for easy readability

--- client/src/components/UI/AddRoom/AddRoom.jsx
+++ client/src/components/UI/AddRoom/AddRoom.jsx
@@ -5,8 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { showNotificationModal } from "../../../store/actions/notification";
 import { FadeLoader } from "react-spinners";
 import Modal from "../Modal/Modal";
-// import UploadImage from "../UploadImage/UploadImage";
-// import ImagePicker from "../ImagePicker/ImagePicker";
+import UploadImage from "../UploadImage/UploadImage";
 import styles from "./AddRoom.module.scss";
 import { log } from "../../../utils/consoleLog";
 import { disableEnableButton } from "../../../utils/disableEnableButton";
@@ -18,13 +17,14 @@ const AddRoom = () => {
   const [roomDescription, setRoomDescription] = useState("");
   const [numberOfBeds, setNumberOfBeds] = useState(undefined);
   const [roomPrice, setRoomPrice] = useState(undefined);
-  // const [roomPhoto, setRoomPhoto] = useState("");
-  let roomPhoto;
-  const [roomId, setRoomId] = useState("");
-  // const [isRoomAddedSuccessfully, setIsRoomAddedSuccessfully] = useState(false);
+  const [roomImage, setRoomImage] = useState("");
+  const [roomId, setRoomId] = useState(null);
+  const [isRoomAddedSuccessfully, setIsRoomAddedSuccessfully] = useState(false);
   const dispatch = useDispatch();
   const token = useSelector((state) => state.auth.token);
   const showModal = useSelector((state) => state.notification.value);
+  // TODO: Consider uploading image using an independent end point
+  // TODO: consider adding room capacity field
 
   const handleRoomName = (event) => {
     setRoomName(event.target.value);
@@ -40,21 +40,23 @@ const AddRoom = () => {
     setRoomPrice(event.target.value);
   };
 
-  // const onSelectRoomPhoto = (photo) => {
-  //   // setRoomPhoto(photo);
-  //   roomPhoto = photo;
-  // };
-
-  console.log(roomPhoto);
-
   // TODO:consider using useEffect hook to monitor whether something is loading or not;
+
+  const freeRoomData = () => {
+    setIsRoomAddedSuccessfully(true);
+    setRoomName("");
+    setRoomDescription("");
+    setNumberOfBeds("");
+    setRoomPrice("");
+    setRoomImage("");
+  };
 
   const requestBodyData = () => {
     return {
       roomDescription: roomDescription,
       numberOfBeds: numberOfBeds,
       roomPrice: roomPrice,
-      roomPicture: roomPhoto,
+      roomPicture: roomImage,
       roomName: roomName,
     };
   };
@@ -76,7 +78,7 @@ const AddRoom = () => {
       }
       setRoomId(response.data.roomId);
       await dispatch(showNotificationModal("Room added successfully"));
-      console.log(roomId);
+      log("RoomId from server: ", roomId);
     };
   };
 
@@ -87,7 +89,7 @@ const AddRoom = () => {
       disableEnableButton("add-room-btn", true);
       await dispatch(addRoom());
       setIsLoading(false);
-      // setIsRoomAddedSuccessfully(true);
+      freeRoomData();
       disableEnableButton("add-room-btn", false);
     } catch (error) {
       setIsLoading(false);
@@ -97,46 +99,6 @@ const AddRoom = () => {
     }
   };
 
-  // const uploadRoomImage = () => {
-  //   return async (dispatch) => {
-  //     let formData = new FormData();
-  //     formData.append("file", roomPhoto.data);
-
-  //     const response = await axios.post(
-  //       `${baseUrl}/upload-room-image/${roomId}`,
-  //       formData,
-  //       {
-  //         headers: {
-  //           "Content-Type": "multipart/form-data",
-  //           Authorization: "Bearer " + token,
-  //         },
-  //       }
-  //     );
-  //     log(response);
-  //     if (response.data.errorMessage) {
-  //       throw new Error(response.data.errorMessage);
-  //     }
-  //     await dispatch(showNotificationModal("Image upload successful"));
-  //   };
-  // };
-
-  // // handle room image upload
-  // const handleRoomImageUpload = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     setIsLoading(true);
-  //     disableEnableButton("room-image-upload-btn", true);
-  //     await dispatch(uploadRoomImage());
-  //     setIsLoading(false);
-  //     disableEnableButton("room-image-upload-btn", false);
-  //   } catch (error) {
-  //     setIsLoading(false);
-  //     setIsError(true);
-  //     await dispatch(showNotificationModal(error.message));
-  //     disableEnableButton("room-image-upload", false);
-  //   }
-  // };
-
   return (
     <Fragment>
       <div className={styles["add-room"]}>
@@ -144,85 +106,78 @@ const AddRoom = () => {
         <div className={styles["add-room__spinner"]}>
           {isLoading && <FadeLoader />}
         </div>
-        {/* {!isRoomAddedSuccessfully && ( */}
-        <div className={styles["add-room__container"]}>
-          <form
-            className={styles["form"]}
-            onSubmit={(event) => handleAddRoomSubmit(event)}
-          >
-            <div className={styles["form__input__group"]}>
-              <label htmlFor="room name">Room name</label>
-              <input
-                type="text"
-                value={roomName}
-                onChange={(event) => handleRoomName(event)}
-                placeholder="Room name"
-                className={styles["form__input__field"]}
-                required
-              />
-            </div>
-            <div className={styles["form__input__group"]}>
-              <label htmlFor="room description">Room Description</label>
-              <textarea
-                type="text"
-                value={roomDescription}
-                onChange={(event) => handleRoomDescription(event)}
-                placeholder="Room Description"
-                className={styles["form__input__field"]}
-                required
-              />
-            </div>
-            <div className={styles["form__input__group"]}>
-              <label htmlFor="number of beds">Number Of Beds</label>
-              <input
-                type="number"
-                value={numberOfBeds}
-                onChange={(event) => handleNumberOfBeds(event)}
-                placeholder="Number Of Beds"
-                className={styles["form__input__field"]}
-                required
-              />
-            </div>
-            <div className={styles["form__input__group"]}>
-              <label htmlFor="room price">Room Price</label>
-              <input
-                type="number"
-                value={roomPrice}
-                onChange={(event) => handleRoomPrice(event)}
-                placeholder="Room Price"
-                className={styles["form__input__field"]}
-                required
-              />
-            </div>
-            <div className={styles["form__btn__container"]}>
-              <button type="submit" id="add-room-btn" className={styles["btn"]}>
-                Add
-              </button>
-            </div>
-          </form>
-        </div>
-        {/* )} */}
-        {/* {isRoomAddedSuccessfully && (
-          <div className={styles["add-room__image__upload"]}>
-            <UploadImage />
-            <ImagePicker onSelect={onSelectRoomPhoto} />
-            {roomPhoto.preview && (
-              <form
-                onSubmit={(event) => handleRoomImageUpload(event)}
-                className={styles["add-room__image__upload__form"]}
-              >
-                 TODO: consider adding upload icon to the button 
+        {!isRoomAddedSuccessfully && (
+          <div className={styles["add-room__container"]}>
+            <form
+              className={styles["form"]}
+              onSubmit={(event) => handleAddRoomSubmit(event)}
+            >
+              <p className={styles["form__heading"]}>Add New Room</p>
+              <div className={styles["form__input__group"]}>
+                <label htmlFor="room name">Room name</label>
+                <input
+                  type="text"
+                  value={roomName}
+                  onChange={(event) => handleRoomName(event)}
+                  placeholder="Room name"
+                  className={styles["form__input__group__field"]}
+                  required
+                />
+              </div>
+              <div className={styles["form__input__group"]}>
+                <label htmlFor="room description">Room Description</label>
+                <textarea
+                  type="text"
+                  value={roomDescription}
+                  onChange={(event) => handleRoomDescription(event)}
+                  placeholder="Room Description"
+                  className={styles["form__input__group__field"]}
+                  required
+                />
+              </div>
+              <div className={styles["form__input__group"]}>
+                <label htmlFor="number of beds">Number Of Beds</label>
+                <input
+                  type="number"
+                  value={numberOfBeds}
+                  onChange={(event) => handleNumberOfBeds(event)}
+                  placeholder="Number Of Beds"
+                  className={styles["form__input__group__field"]}
+                  required
+                />
+              </div>
+              <div className={styles["form__input__group"]}>
+                <label htmlFor="room price">Room Price</label>
+                <input
+                  type="number"
+                  value={roomPrice}
+                  onChange={(event) => handleRoomPrice(event)}
+                  placeholder="Room Price"
+                  className={styles["form__input__group__field"]}
+                  required
+                />
+              </div>
+              <div className={styles["form__btn__container"]}>
                 <button
                   type="submit"
-                  id="room-image-upload-btn"
+                  id="add-room-btn"
                   className={styles["btn"]}
                 >
-                  Upload
+                  Add Room
                 </button>
-              </form>
-            )}
+              </div>
+            </form>
           </div>
-        )} */}
+        )}
+        {isRoomAddedSuccessfully && (
+          <div className={styles["add-room__upload--image"]}>
+            <UploadImage
+              apiEndpoint={"upload-room-image"}
+              id={roomId}
+              category={"Room"}
+            />
+          </div>
+        )}
       </div>
     </Fragment>
   );
