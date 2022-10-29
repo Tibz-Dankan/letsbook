648743a5ad30a337c2a7cc6cc88b84d4f8d27172
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-10-29 13:50:28 +0300

    refactor: UI appearance

--- client/src/components/UI/EditRoom/EditRoom.jsx
+++ client/src/components/UI/EditRoom/EditRoom.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
 import ReactModal from "react-modal";
 import { FadeLoader } from "react-spinners";
+import { RiCloseLine } from "react-icons/ri";
 import { editRoom } from "../../../store/actions/room";
 import { useSelector, useDispatch } from "react-redux";
 import styles from "./EditRoom.module.scss";
@@ -13,22 +14,23 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
+    backgroundColor: "lightgrey",
   },
 };
 
 // ReactModal.setAppElement("#yourAppElement");
 
-const EditRoom = ({ roomObject }) => {
-  const [roomName, setRoomName] = useState(roomObject.room_name);
+const EditRoom = ({ roomDataObject }) => {
+  const [roomName, setRoomName] = useState(roomDataObject.room_name);
   const [roomDescription, setRoomDescription] = useState(
-    roomObject.room_description
+    roomDataObject.room_description
   );
-  const [noOfBeds, setNoOfBeds] = useState(roomObject.no_of_beds);
-  const [price, setPrice] = useState(roomObject.price);
+  const [noOfBeds, setNoOfBeds] = useState(roomDataObject.no_of_beds);
+  const [price, setPrice] = useState(roomDataObject.price);
   const [roomCapacityNum, setRoomCapacityNum] = useState(
-    roomObject.room_capacity_num
+    roomDataObject.room_capacity_num
   );
-  const roomId = roomObject.room_id;
+  const roomId = roomDataObject.room_id;
   const authToken = useSelector((state) => state.auth.token);
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
@@ -72,7 +74,6 @@ const EditRoom = ({ roomObject }) => {
     }
   };
 
-  let subtitle;
   const [modalIsOpen, setIsOpen] = useState(false);
 
   const openModal = () => {
@@ -81,7 +82,7 @@ const EditRoom = ({ roomObject }) => {
 
   const afterOpenModal = () => {
     // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
+    // subtitle.style.color = "#f00";
   };
 
   const closeModal = () => {
@@ -92,7 +93,7 @@ const EditRoom = ({ roomObject }) => {
     <Fragment>
       <div className={styles["edit-room"]}>
         <div className={styles["edit-room__open-modal-btn"]}>
-          <button onClick={() => openModal()}>Edit</button>
+          <button onClick={() => openModal()}>Edit Room Details</button>
         </div>
         <ReactModal
           isOpen={modalIsOpen}
@@ -102,17 +103,16 @@ const EditRoom = ({ roomObject }) => {
           ariaHideApp={false}
           contentLabel="Example Modal"
         >
-          <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Edit Room</h2>
-          <button
-            className={styles["edit-room__close-modal-btn"]}
-            onClick={() => closeModal()}
-          >
-            close
-          </button>
-          <div className={styles["edit-room__form__container"]}>
+          <div className={styles["edit-room__content"]}>
+            <button
+              className={styles["edit-room__content-close-modal-btn"]}
+              onClick={() => closeModal()}
+            >
+              <RiCloseLine />
+            </button>
             {isLoading && (
-              <div className={styles["edit-room__loader"]}>
-                <FadeLoader />
+              <div className={styles["edit-room__content__loader"]}>
+                <FadeLoader color="hsl(266, 50%, 36%)" />
                 <span>Updating Room...</span>
               </div>
             )}
@@ -120,8 +120,8 @@ const EditRoom = ({ roomObject }) => {
               className={styles["form"]}
               onSubmit={(event) => handleUpdatedRoomDataSubmit(event)}
             >
-              <div className={["form__heading"]}>
-                <h4>Edit Room</h4>
+              <div className={styles["form__heading"]}>
+                <p>Edit {roomDataObject.room_name} Details</p>
               </div>
               <div className={styles["form__group"]}>
                 <label className={styles["form__group__label"]}>
@@ -140,20 +140,14 @@ const EditRoom = ({ roomObject }) => {
                 <label className={styles["form__group__label"]}>
                   Room Description
                 </label>
-                <textarea className={styles["form__group__input"]}
-                  type="text"
-                  placeholder="Room Name"
-                  value={roomDescription}
-                  onChange={(event) => handleRoomDescriptionChange(event)}
-                  required/>
-                {/* <input
+                <textarea
                   className={styles["form__group__input"]}
                   type="text"
                   placeholder="Room Name"
                   value={roomDescription}
                   onChange={(event) => handleRoomDescriptionChange(event)}
                   required
-                /> */}
+                />
               </div>
               <div className={styles["form__group"]}>
                 <label className={styles["form__group__label"]}>
