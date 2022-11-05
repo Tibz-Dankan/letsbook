3320db49e62bc9b784445b6b94bb9611d7f0607e
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-11-05 12:35:06 +0300

    fix(MulterError): fieldSize increased to allow image files of  max-size 10mbs

--- server/src/utils/multer.js
+++ server/src/utils/multer.js
@@ -1,6 +1,9 @@
 const multer = require("multer");
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fieldSize: 10 * 1024 * 1024 },
+});
 
 module.exports = { upload };
